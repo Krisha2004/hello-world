7924d494069e68f2705ddaf508e945ed09846a25
Krisha2004 <89457159+Krisha2004@users.noreply.github.com>
2021-09-02 15:47:49 +0100

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,7 @@
+import random
+a = ["hot", "summer", "hard", "dry", "simple", "light", "weak", "male", "sad", "win", "small","ignore", "buy", "succeed", "reject", "prevent", "exclude"]
+
+b = ["cold", "winter", "soft", "wet", "complex", "darkness", "strong", "female", "happy", "lose", "big", "pay attention", "sell", "fail", "accept", "allow", "include"]
+s = random.choice(a)
+a = random.choice(b)
+print(s +(' is to '))
